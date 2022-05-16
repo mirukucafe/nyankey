6060e7d22053eb1d99262737632fa1f7b16195c2
Johann150 <johann.galle@protonmail.com>
2022-05-16 09:18:09 +0200

    enhance: better rendering of reports
    Passing the report object reduces the number of parameters to be passed.

--- packages/backend/src/remote/activitypub/renderer/flag.ts
+++ packages/backend/src/remote/activitypub/renderer/flag.ts
@@ -1,13 +1,12 @@
 import config from '@/config/index.js';
 import { ILocalUser } from '@/models/entities/user.js';
+import { AbuseUserReport } from '@/models/entities/abuse-user-report.js';
 
-// to anonymise reporters, the reporting actor must be a system user
-// object has to be a uri or array of uris
-export const renderFlag = (user: ILocalUser, object: [string], content: string) => {
+export const renderFlag = (user: ILocalUser, flag: AbuseUserReport) => {
 	return {
 		type: 'Flag',
 		actor: `${config.url}/users/${user.id}`,
-		content,
-		object,
+		content: flag.comment,
+		object: flag.urls,
 	};
 };
