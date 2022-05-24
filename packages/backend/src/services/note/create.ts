cfa371b52b980991946ce0f3353479f0cc26dcc6
Johann150 <johann.galle@protonmail.com>
2022-05-24 10:00:46 +0200

    refactor: remove note re-packing in streaming API
    Instead of packing the note for public user before passing it to
    streams, the note is now either packed for the user the respective
    stream belongs to (`mainStream`) or not packed at all and then packed
    later (`notesStream`).
    
    Because this is a new common task between different channels, a shared
    implementation of packing a note from notesStream is created. This
    implementation will simply skip a note if it is not visible to the user
    that the channel belongs to.

--- packages/backend/src/services/note/create.ts
+++ packages/backend/src/services/note/create.ts
@@ -345,19 +345,15 @@ export default async (user: { id: User['id']; username: User['username']; host: 
 			}
 		}
 
-		// Pack the note
-		const noteObj = await Notes.pack(note);
+		publishNotesStream(note);
 
-		publishNotesStream(noteObj);
+		const webhooks = await getActiveWebhooks().then(webhooks => webhooks.filter(x => x.userId === user.id && x.on.includes('note')));
 
-		getActiveWebhooks().then(webhooks => {
-			webhooks = webhooks.filter(x => x.userId === user.id && x.on.includes('note'));
-			for (const webhook of webhooks) {
-				webhookDeliver(webhook, 'note', {
-					note: noteObj,
-				});
-			}
-		});
+		for (const webhook of webhooks) {
+			webhookDeliver(webhook, 'note', {
+				note: await Notes.pack(note, user),
+			});
+		}
 
 		const nm = new NotificationManager(user, note);
 		const nmRelatedPromises = [];
@@ -378,12 +374,14 @@ export default async (user: { id: User['id']; username: User['username']; host: 
 
 				if (!threadMuted) {
 					nm.push(data.reply.userId, 'reply');
-					publishMainStream(data.reply.userId, 'reply', noteObj);
+
+					const packedReply = await Notes.pack(note, { id: data.reply.userId });
+					publishMainStream(data.reply.userId, 'reply', packedReply);
 
 					const webhooks = (await getActiveWebhooks()).filter(x => x.userId === data.reply!.userId && x.on.includes('reply'));
 					for (const webhook of webhooks) {
 						webhookDeliver(webhook, 'reply', {
-							note: noteObj,
+							note: packedReply,
 						});
 					}
 				}
@@ -404,12 +402,13 @@ export default async (user: { id: User['id']; username: User['username']; host: 
 
 			// Publish event
 			if ((user.id !== data.renote.userId) && data.renote.userHost === null) {
-				publishMainStream(data.renote.userId, 'renote', noteObj);
+				const packedRenote = await Notes.pack(note, { id: data.renote.userId });
+				publishMainStream(data.renote.userId, 'renote', packedRenote);
 
 				const webhooks = (await getActiveWebhooks()).filter(x => x.userId === data.renote!.userId && x.on.includes('renote'));
 				for (const webhook of webhooks) {
 					webhookDeliver(webhook, 'renote', {
-						note: noteObj,
+						note: packedRenote,
 					});
 				}
 			}
