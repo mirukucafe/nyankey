95a8a937e2ce5d115be0d861103bfcb613429d50
Johann150 <johann.galle@protonmail.com>
2022-06-29 22:09:44 +0200

    fix client router catchall
    fixes #8903

--- packages/client/src/router.ts
+++ packages/client/src/router.ts
@@ -239,7 +239,7 @@ export const routes = [{
 	component: $i ? page(() => import('./pages/timeline.vue')) : page(() => import('./pages/welcome.vue')),
 	globalCacheKey: 'index',
 }, {
-	path: '/(*)',
+	path: '/:(*)',
 	component: page(() => import('./pages/not-found.vue')),
 }];
 
