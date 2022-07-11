3317e21b39354dedd7b9d9cc03cc6fa362b08534
Johann150 <johann.galle@protonmail.com>
2022-07-11 15:45:12 +0200

    chore: remove unused imports

--- packages/backend/src/queue/processors/db/export-custom-emojis.ts
+++ packages/backend/src/queue/processors/db/export-custom-emojis.ts
@@ -1,7 +1,6 @@
 import Bull from 'bull';
 import * as fs from 'node:fs';
 
-import { ulid } from 'ulid';
 import mime from 'mime-types';
 import archiver from 'archiver';
 import { queueLogger } from '../../logger.js';
