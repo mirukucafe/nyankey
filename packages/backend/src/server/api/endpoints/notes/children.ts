4bc9610d8bf5af736b5e89e4782395705de45d7d
Johann150 <johann.galle@protonmail.com>
2022-07-25 21:46:45 +0200

    remove unnecessary joins
    These joins are no longer necessary as of commit
    c35372a20d22cddb75e93a0b407f2b652cd7faf0. It seems they are bad enough
    for performance to break installs.

--- packages/backend/src/server/api/endpoints/notes/children.ts
+++ packages/backend/src/server/api/endpoints/notes/children.ts
@@ -55,14 +55,6 @@ export default define(meta, paramDef, async (ps, user) => {
 		.innerJoinAndSelect('note.user', 'user')
 		.leftJoinAndSelect('user.avatar', 'avatar')
 		.leftJoinAndSelect('user.banner', 'banner')
-		.leftJoinAndSelect('note.reply', 'reply')
-		.leftJoinAndSelect('note.renote', 'renote')
-		.leftJoinAndSelect('reply.user', 'replyUser')
-		.leftJoinAndSelect('replyUser.avatar', 'replyUserAvatar')
-		.leftJoinAndSelect('replyUser.banner', 'replyUserBanner')
-		.leftJoinAndSelect('renote.user', 'renoteUser')
-		.leftJoinAndSelect('renoteUser.avatar', 'renoteUserAvatar')
-		.leftJoinAndSelect('renoteUser.banner', 'renoteUserBanner');
 
 	generateVisibilityQuery(query, user);
 	if (user) {
