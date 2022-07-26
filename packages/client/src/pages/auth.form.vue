ddebf5a049ffaa37da3dee86fbf170b8959ff708
Johann150 <johann.galle@protonmail.com>
2022-07-26 13:53:15 +0200

    clean up refactor: use i18n and remove unused import

--- packages/client/src/pages/auth.form.vue
+++ packages/client/src/pages/auth.form.vue
@@ -1,28 +1,29 @@
 <template>
 <section class="_section">
-	<div class="_title">{{ $t('_auth.shareAccess', { name: app.name }) }}</div>
+	<div class="_title">{{ i18n.t('_auth.shareAccess', { name: app.name }) }}</div>
 	<div class="_content">
 		<h2>{{ app.name }}</h2>
 		<p class="id">{{ app.id }}</p>
 		<p class="description">{{ app.description }}</p>
 	</div>
 	<div class="_content">
-		<h2>{{ $ts._auth.permissionAsk }}</h2>
+		<h2>{{ i18n.ts._auth.permissionAsk }}</h2>
 		<ul>
-			<li v-for="p in app.permission" :key="p">{{ $t(`_permissions.${p}`) }}</li>
+			<li v-for="p in app.permission" :key="p">{{ i18n.t(`_permissions.${p}`) }}</li>
 		</ul>
 	</div>
 	<div class="_footer">
-		<MkButton inline @click="cancel">{{ $ts.cancel }}</MkButton>
-		<MkButton inline primary @click="accept">{{ $ts.accept }}</MkButton>
+		<MkButton inline @click="cancel">{{ i18n.ts.cancel }}</MkButton>
+		<MkButton inline primary @click="accept">{{ i18n.ts.accept }}</MkButton>
 	</div>
 </section>
 </template>
 
 <script lang="ts" setup>
-import { defineComponent } from 'vue';
+import { } from 'vue';
 import MkButton from '@/components/ui/button.vue';
 import * as os from '@/os';
+import { i18n } from '@/i18n';
 
 const emit = defineEmits<{
 	(ev: 'denied'): void;
