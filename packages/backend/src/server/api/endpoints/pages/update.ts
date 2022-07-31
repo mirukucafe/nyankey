90d35b951f4f78c846b21ba81fdee5e465e06f8a
Johann150 <johann.galle@protonmail.com>
2022-07-31 21:28:48 +0200

    adjust types & api for pages

--- packages/backend/src/server/api/endpoints/pages/update.ts
+++ packages/backend/src/server/api/endpoints/pages/update.ts
@@ -49,19 +49,13 @@ export const paramDef = {
 		title: { type: 'string' },
 		name: { type: 'string', minLength: 1 },
 		summary: { type: 'string', nullable: true },
-		content: { type: 'array', items: {
-			type: 'object', additionalProperties: true,
-		} },
-		variables: { type: 'array', items: {
-			type: 'object', additionalProperties: true,
-		} },
-		script: { type: 'string' },
+		text: { type: 'string', minLength: 1 },
 		eyeCatchingImageId: { type: 'string', format: 'misskey:id', nullable: true },
 		font: { type: 'string', enum: ['serif', 'sans-serif'] },
 		alignCenter: { type: 'boolean' },
 		hideTitleWhenPinned: { type: 'boolean' },
 	},
-	required: ['pageId', 'title', 'name', 'content', 'variables', 'script'],
+	required: ['pageId', 'title', 'name', 'text'],
 } as const;
 
 // eslint-disable-next-line import/no-default-export
@@ -101,9 +95,7 @@ export default define(meta, paramDef, async (ps, user) => {
 		title: ps.title,
 		name: ps.name === undefined ? page.name : ps.name,
 		summary: ps.name === undefined ? page.summary : ps.summary,
-		content: ps.content,
-		variables: ps.variables,
-		script: ps.script,
+		text: ps.text,
 		alignCenter: ps.alignCenter === undefined ? page.alignCenter : ps.alignCenter,
 		hideTitleWhenPinned: ps.hideTitleWhenPinned === undefined ? page.hideTitleWhenPinned : ps.hideTitleWhenPinned,
 		font: ps.font === undefined ? page.font : ps.font,
