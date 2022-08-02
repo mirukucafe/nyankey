fbcea23ef6ef125cdb974324f29c612b342ddae3
Johann150 <johann.galle@protonmail.com>
2022-08-02 21:23:16 +0200

    fix ReferenceError in renote.ts

--- packages/backend/src/misc/renote.ts
+++ packages/backend/src/misc/renote.ts
@@ -1,5 +1,5 @@
 import { Note } from '@/models/entities/note.js';
 
 export function isPureRenote(note: Note): boolean {
-	return note.renoteId != null && note.text == null && (renote.fileIds == null || renote.fileIds.length === 0) && !note.hasPoll;
+	return note.renoteId != null && note.text == null && (note.fileIds == null || note.fileIds.length === 0) && !note.hasPoll;
 }
