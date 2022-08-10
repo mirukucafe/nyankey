mirukucafe/nyankey

commit 7a8001522548f92f08e8cb6ffcf5f908a1db6c05

fix lint "no-param-reassign"

packages/backend/src/remote/resolve-user.ts
@@ -2,7 +2,7 @@ import { URL } from 'node:url';
 import chalk from 'chalk';
 import { IsNull } from 'typeorm';
 import config from '@/config/index.js';
-import { toPuny } from '@/misc/convert-host.js';
+import { isSelfHost, toPuny } from '@/misc/convert-host.js';
 import { User, IRemoteUser } from '@/models/entities/user.js';
 import { Users } from '@/models/index.js';
 import webFinger from './webfinger.js';
@@ -11,7 +11,7 @@ import { remoteLogger } from './logger.js';
 
 const logger = remoteLogger.createSubLogger('resolve-user');
 
-export async function resolveUser(username: string, host: string | null): Promise<User> {
+export async function resolveUser(username: string, idnHost: string | null): Promise<User> {
 	const usernameLower = username.toLowerCase();
 
 	if (host == null) {
@@ -25,9 +25,7 @@ export async function resolveUser(username: string, host: string | null): Promis
 		});
 	}
 
-	host = toPuny(host);
-
-	if (config.host === host) {
+	if (isSelfHost(idnHost)) {
 		logger.info(`return local user: ${usernameLower}`);
 		return await Users.findOneBy({ usernameLower, host: IsNull() }).then(u => {
 			if (u == null) {
@@ -38,6 +36,8 @@ export async function resolveUser(username: string, host: string | null): Promis
 		});
 	}
 
+	// `idnHost` can not be null here because that would have branched off with `isSelfHost`.
+	const host = toPuny(idnHost!);
 	const user = await Users.findOneBy({ usernameLower, host }) as IRemoteUser | null;
 
 	const acctLower = `${usernameLower}@${host}`;

packages/backend/src/mfm/from-html.ts
@@ -8,10 +8,10 @@ const urlRegex = /^https?:\/\/[\w\/:%#@$&?!()\[\]~.,=+\-]+/;
 const urlRegexFull = /^https?:\/\/[\w\/:%#@$&?!()\[\]~.,=+\-]+$/;
 
 export function fromHtml(html: string, hashtagNames?: string[]): string {
-	// some AP servers like Pixelfed use br tags as well as newlines
-	html = html.replace(/<br\s?\/?>\r?\n/gi, '\n');
-
-	const dom = parse5.parseFragment(html);
+	const dom = parse5.parseFragment(
+		// some AP servers like Pixelfed use br tags as well as newlines
+		html.replace(/<br\s?\/?>\r?\n/gi, '\n')
+	);
 
 	let text = '';
 

packages/backend/src/misc/acct.ts
@@ -4,8 +4,11 @@ export type Acct = {
 };
 
 export function parse(acct: string): Acct {
-	if (acct.startsWith('@')) acct = acct.substr(1);
-	const split = acct.split('@', 2);
+	const split = acct.split('@');
+	if (split[0].length == 0) {
+		// there was an initial at
+		split.shift();
+	}
 	return { username: split[0], host: split[1] || null };
 }
 

packages/backend/src/misc/convert-host.ts
@@ -2,11 +2,11 @@ import { URL } from 'node:url';
 import { toASCII } from 'punycode';
 import config from '@/config/index.js';
 
-export function getFullApAccount(username: string, host: string | null) {
+export function getFullApAccount(username: string, host: string | null): string {
 	return host ? `${username}@${toPuny(host)}` : `${username}@${toPuny(config.host)}`;
 }
 
-export function isSelfHost(host: string) {
+export function isSelfHost(host: string | null): boolean {
 	if (host == null) return true;
 	return toPuny(config.host) === toPuny(host);
 }

packages/backend/src/misc/gen-id.ts
@@ -7,10 +7,8 @@ import * as crypto from 'node:crypto';
 const TIME2000 = 946684800000;
 let counter = crypto.randomBytes(2).readUInt16LE(0);
 
-export function genId(date?: Date): string {
-	if (!date || (date > new Date())) date = new Date();
-
-	let t = date.getTime();
+export function genId(date?: Date = new Date()): string {
+	let t = Math.min(date, new Date());
 	t -= TIME2000;
 	if (t < 0) t = 0;
 	if (isNaN(t)) throw new Error('Failed to create AID: Invalid Date');

packages/backend/src/misc/reaction-lib.ts
@@ -54,10 +54,10 @@ export function convertLegacyReactions(reactions: Record<string, number>) {
 	return _reactions2;
 }
 
-export async function toDbReaction(reaction?: string | null, reacterHost?: string | null): Promise<string> {
+export async function toDbReaction(reaction?: string | null, idnReacterHost?: string | null): Promise<string> {
 	if (reaction == null) return await getFallbackReaction();
 
-	reacterHost = toPunyNullable(reacterHost);
+	const reacterHost = toPunyNullable(idnReacterHost);
 
 	// 文字列タイプのリアクションを絵文字に変換
 	if (Object.keys(legacies).includes(reaction)) return legacies[reaction];
@@ -124,8 +124,8 @@ export function decodeReaction(str: string): DecodedReaction {
 	};
 }
 
-export function convertLegacyReaction(reaction: string): string {
-	reaction = decodeReaction(reaction).reaction;
+export function convertLegacyReaction(_reaction: string): string {
+	const reaction = decodeReaction(_reaction).reaction;
 	if (Object.keys(legacies).includes(reaction)) return legacies[reaction];
 	return reaction;
 }

packages/backend/src/models/repositories/following.ts
@@ -52,11 +52,9 @@ export const FollowingRepository = db.getRepository(Following).extend({
 		opts?: {
 			populateFollowee?: boolean;
 			populateFollower?: boolean;
-		}
+		} = {},
 	): Promise<Packed<'Following'>> {
 		const following = typeof src === 'object' ? src : await this.findOneByOrFail({ id: src });
-
-		if (opts == null) opts = {};
 
 		return await awaitAll({
 			id: following.id,

packages/backend/src/remote/activitypub/models/note.ts
@@ -65,9 +65,7 @@ export async function fetchNote(object: string | IObject): Promise<Note | null> 
 /**
  * Noteを作成します。
  */
-export async function createNote(value: string | IObject, resolver?: Resolver, silent = false): Promise<Note | null> {
-	if (resolver == null) resolver = new Resolver();
-
+export async function createNote(value: string | IObject, resolver?: Resolver = new Resolver(), silent = false): Promise<Note | null> {
 	const object: any = await resolver.resolve(value);
 
 	const entryUri = getApId(value);
@@ -302,8 +300,8 @@ export async function resolveNote(value: string | IObject, resolver?: Resolver):
 	}
 }
 
-export async function extractEmojis(tags: IObject | IObject[], host: string): Promise<Emoji[]> {
-	host = toPuny(host);
+export async function extractEmojis(tags: IObject | IObject[], idnHost: string): Promise<Emoji[]> {
+	const host = toPuny(idnHost);
 
 	if (!tags) return [];
 

commit 961fb0d2df7c4910ba97341cf8b167ff1feea42e

fix: use correct variable
Fixes an error introduced in commit 7a8001522548f92f08e8cb6ffcf5f908a1db6c05.

packages/backend/src/remote/resolve-user.ts
@@ -14,7 +14,7 @@ const logger = remoteLogger.createSubLogger('resolve-user');
 export async function resolveUser(username: string, idnHost: string | null): Promise<User> {
 	const usernameLower = username.toLowerCase();
 
-	if (host == null) {
+	if (idnHost == null) {
 		logger.info(`return local user: ${usernameLower}`);
 		return await Users.findOneBy({ usernameLower, host: IsNull() }).then(u => {
 			if (u == null) {