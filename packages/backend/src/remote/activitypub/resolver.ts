8e5a350f1ba0c56ef6b2df3b7237f3537ec4f36a
Francis Dinh <normandy@biribiri.dev>
2022-08-21 13:13:34 -0400

    Simplify fetching object in resolver.ts
    this.user will always be true, so we aren't doing unsigned fetches.

--- packages/backend/src/remote/activitypub/resolver.ts
+++ packages/backend/src/remote/activitypub/resolver.ts
@@ -76,9 +76,7 @@ export default class Resolver {
 			this.user = await getInstanceActor();
 		}
 
-		const object = (this.user
-			? await signedGet(value, this.user)
-			: await getJson(value, 'application/activity+json, application/ld+json')) as IObject;
+		const object = await signedGet(value, this.user);
 
 		if (object == null || (
 			Array.isArray(object['@context']) ?
