a16d7d9dc260721f9939131aea0971422604abd9
Francis Dinh <normandy@biribiri.dev>
2022-09-06 11:36:21 -0400

    foundkey-js: fix no-param-reassign lint in acct.ts

--- packages/foundkey-js/src/acct.ts
+++ packages/foundkey-js/src/acct.ts
@@ -4,8 +4,8 @@ export type Acct = {
 };
 
 export function parse(acct: string): Acct {
-	if (acct.startsWith('@')) acct = acct.substr(1);
-	const split = acct.split('@', 2);
+	const acct_ = acct.startsWith('@') ? acct.slice(1) : acct;
+	const split = acct_.split('@', 2);
 	return { username: split[0], host: split[1] || null };
 }
 
