fa0dce6439d838bb380205f0465de50f639300b9
Francis Dinh <normandy@biribiri.dev>
2022-09-23 11:48:10 -0400

    Remove 'recursive: true' from removing tsbuildinfo

--- scripts/clean.js
+++ scripts/clean.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 fs.rmSync(__dirname + '/../packages/backend/built', { recursive: true, force: true });
-fs.rmSync(__dirname + '/../packages/backend/tsconfig.tsbuildinfo', { recursive: true, force: true });
+fs.rmSync(__dirname + '/../packages/backend/tsconfig.tsbuildinfo', { force: true });
 fs.rmSync(__dirname + '/../packages/client/built', { recursive: true, force: true });
 fs.rmSync(__dirname + '/../packages/foundkey-js/built', { recursive: true, force: true });
 fs.rmSync(__dirname + '/../packages/sw/built', { recursive: true, force: true });
