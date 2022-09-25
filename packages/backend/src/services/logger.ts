23fbdfdf1f4303fe7be422d65e8e4ca0c16774b8
Michcio <public+git@meekchopp.es>
2022-09-25 16:29:20 +0200

    Fix typos in syslog initialization

--- packages/backend/src/services/logger.ts
+++ packages/backend/src/services/logger.ts
@@ -28,9 +28,9 @@ export default class Logger {
 
 		if (config.syslog) {
 			this.syslogClient = new SyslogPro.RFC5424({
-				applacationName: 'FoundKey',
+				applicationName: 'FoundKey',
 				timestamp: true,
-				encludeStructuredData: true,
+				includeStructuredData: true,
 				color: true,
 				extendedColor: true,
 				server: {
