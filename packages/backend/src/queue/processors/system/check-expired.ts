5b7a06675fac535ff434db220b9c5fd090cf3cea
Johann150 <johann.galle@protonmail.com>
2022-09-30 17:36:57 +0200

    refactor expiring data and expire signins after 60 days
    closes https://akkoma.dev/FoundKeyGang/FoundKey/issues/176
    
    Changelog: Added

---
+++ packages/backend/src/queue/processors/system/check-expired.ts
@@ -0,0 +1,41 @@
+import Bull from 'bull';
+import { In, LessThan } from 'typeorm';
+import { AttestationChallenges, Mutings, Signins } from '@/models/index.js';
+import { publishUserEvent } from '@/services/stream.js';
+import { MINUTE, DAY } from '@/const.js';
+import { queueLogger } from '@/queue/logger.js';
+
+const logger = queueLogger.createSubLogger('check-expired');
+
+export async function checkExpired(job: Bull.Job<Record<string, unknown>>, done: any): Promise<void> {
+	logger.info('Checking expired data...');
+
+	const expiredMutings = await Mutings.createQueryBuilder('muting')
+		.where('muting.expiresAt IS NOT NULL')
+		.andWhere('muting.expiresAt < :now', { now: new Date() })
+		.innerJoinAndSelect('muting.mutee', 'mutee')
+		.getMany();
+
+	if (expiredMutings.length > 0) {
+		await Mutings.delete({
+			id: In(expiredMutings.map(m => m.id)),
+		});
+
+		for (const m of expiredMutings) {
+			publishUserEvent(m.muterId, 'unmute', m.mutee!);
+		}
+	}
+
+	await Signins.delete({
+		// 60 days, or roughly equal to two months
+		createdAt: LessThan(new Date(new Date().getTime() - 60 * DAY)),
+	});
+
+	await AttestationChallenges.delete({
+		createdAt: LessThan(new Date(new Date().getTime() - 5 * MINUTE)),
+	});
+
+	logger.succ('Deleted expired mutes, signins and attestation challenges.');
+
+	done();
+}
--- packages/backend/src/queue/processors/system/index.ts
+++ packages/backend/src/queue/processors/system/index.ts
@@ -2,13 +2,13 @@ import Bull from 'bull';
 import { tickCharts } from './tick-charts.js';
 import { resyncCharts } from './resync-charts.js';
 import { cleanCharts } from './clean-charts.js';
-import { checkExpiredMutings } from './check-expired-mutings.js';
+import { checkExpired } from './check-expired.js';
 
 const jobs = {
 	tickCharts,
 	resyncCharts,
 	cleanCharts,
-	checkExpiredMutings,
+	checkExpired,
 } as Record<string, Bull.ProcessCallbackFunction<Record<string, unknown>> | Bull.ProcessPromiseFunction<Record<string, unknown>>>;
 
 export default function(dbQueue: Bull.Queue<Record<string, unknown>>) {
