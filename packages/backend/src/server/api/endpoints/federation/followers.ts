b2c483faf5519ffa5bbcd26742f6af436e028341
Atsuko Karagi <atsuko.karagi@localhost>
2022-10-22 17:19:36 +0200

    backend: tweak endpoint permissions
    Changelog: Changed

--- packages/backend/src/server/api/endpoints/federation/followers.ts
+++ packages/backend/src/server/api/endpoints/federation/followers.ts
@@ -5,7 +5,8 @@ import { makePaginationQuery } from '../../common/make-pagination-query.js';
 export const meta = {
 	tags: ['federation'],
 
-	requireCredential: false,
+	requireCredential: true,
+	requireAdmin: true,
 
 	res: {
 		type: 'array',
