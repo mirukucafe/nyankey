a3468491a7b72e1b2228aab3f1e1daf7984a045a
Johann150 <johann.galle@protonmail.com>
2022-11-12 18:51:57 +0100

    fix import

--- packages/backend/src/services/suspend-user.ts
+++ packages/backend/src/services/suspend-user.ts
@@ -1,7 +1,7 @@
 import { Not, IsNull } from 'typeorm';
 import renderDelete from '@/remote/activitypub/renderer/delete.js';
 import { renderActivity } from '@/remote/activitypub/renderer/index.js';
-import { DeliverManager } from '@/remote/activitypub/deliver-manager.js';
+import DeliverManager from '@/remote/activitypub/deliver-manager.js';
 import config from '@/config/index.js';
 import { User } from '@/models/entities/user.js';
 import { Users, Followings } from '@/models/index.js';
