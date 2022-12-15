84d83d908a84fd5e6d9d05c6a14035e345ceaa72
Johann150 <johann.galle@protonmail.com>
2022-12-15 17:52:19 +0100

    client: add button to unrenote
    Changelog: Added

--- packages/client/src/components/renote-button.vue
+++ packages/client/src/components/renote-button.vue
@@ -62,6 +62,22 @@ function renote(viaKeyboard = false): void {
 			});
 		},
 	}, {
+		text: i18n.ts.unrenoteAll,
+		icon: 'fas fa-trash-alt',
+		danger: true,
+		action: () => {
+			os.confirm({
+				type: 'warning',
+				text: i18n.ts.unrenoteAllConfirm,
+			}).then(({ canceled }) => {
+				if (canceled) return;
+
+				os.api('notes/unrenote', {
+					noteId: props.note.id,
+				});
+			});
+		},
+	}, {
 		text: i18n.ts.quote,
 		icon: 'fas fa-quote-right',
 		action: () => {
