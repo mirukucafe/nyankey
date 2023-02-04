839daea8877114a2d1b4116553dd8fefb0360986
Johann150 <johann.galle@protonmail.com>
2023-02-04 18:08:19 +0100

    remove mi-white.png asset

--- packages/backend/src/services/send-email.ts
+++ packages/backend/src/services/send-email.ts
@@ -8,7 +8,7 @@ export const logger = new Logger('email');
 export async function sendEmail(to: string, subject: string, html: string, text: string): Promise<void> {
 	const meta = await fetchMeta(true);
 
-	const iconUrl = `${config.url}/static-assets/mi-white.png`;
+	const iconUrl = `${config.url}/static-assets/icons/512.png`;
 	const emailSettingUrl = `${config.url}/settings/email`;
 
 	const enableAuth = meta.smtpUser != null && meta.smtpUser !== '';
