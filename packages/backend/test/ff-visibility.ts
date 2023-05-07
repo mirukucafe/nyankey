3d4df807b0e4465f3b23b2589e2f4472f78f8006
Johann150 <johann.galle@protonmail.com>
2023-05-07 23:35:29 +0200

    tests: refactor ffVisibility tests
    Using some closures to partly generate the tests to achieve better coverage

--- packages/backend/test/ff-visibility.ts
+++ packages/backend/test/ff-visibility.ts
@@ -9,159 +9,108 @@ describe('FF visibility', () => {
 
 	let alice: any;
 	let bob: any;
-	let carol: any;
+	let follower: any;
 
 	before(async () => {
 		p = await startServer();
 		alice = await signup({ username: 'alice' });
 		bob = await signup({ username: 'bob' });
-		carol = await signup({ username: 'carol' });
+		follower = await signup({ username: 'follower' });
+
+		await request('/following/create', { userId: alice.id }, follower);
 	});
 
 	after(async () => {
 		await shutdownServer(p);
 	});
 
-	it('ffVisibility が public なユーザーのフォロー/フォロワーを誰でも見れる', async(async () => {
-		await request('/i/update', {
-			ffVisibility: 'public',
-		}, alice);
+	const visible = (user) => {
+		return async () => {
+			const followingRes = await request('/users/following', {
+				userId: alice.id,
+			}, user);
+			const followersRes = await request('/users/followers', {
+				userId: alice.id,
+			}, user);
 
-		const followingRes = await request('/users/following', {
-			userId: alice.id,
-		}, bob);
-		const followersRes = await request('/users/followers', {
-			userId: alice.id,
-		}, bob);
+			assert.strictEqual(followingRes.status, 200);
+			assert.ok(Array.isArray(followingRes.body));
+			assert.strictEqual(followersRes.status, 200);
+			assert.ok(Array.isArray(followersRes.body));
+		};
+	};
 
-		assert.strictEqual(followingRes.status, 200);
-		assert.strictEqual(Array.isArray(followingRes.body), true);
-		assert.strictEqual(followersRes.status, 200);
-		assert.strictEqual(Array.isArray(followersRes.body), true);
-	}));
+	const hidden = (user) => {
+		return async () => {
+			const followingRes = await request('/users/following', {
+				userId: alice.id,
+			}, user);
+			const followersRes = await request('/users/followers', {
+				userId: alice.id,
+			}, user);
 
-	it('ffVisibility が followers なユーザーのフォロー/フォロワーを自分で見れる', async(async () => {
-		await request('/i/update', {
-			ffVisibility: 'followers',
-		}, alice);
+			assert.strictEqual(followingRes.status, 403);
+			assert.strictEqual(followersRes.status, 403);
+		};
+	};
 
-		const followingRes = await request('/users/following', {
-			userId: alice.id,
-		}, alice);
-		const followersRes = await request('/users/followers', {
-			userId: alice.id,
-		}, alice);
+	describe('public visibility', () => {
+		before(async () => {
+			await request('/i/update', {
+				ffVisibility: 'public',
+			}, alice);
+		});
 
-		assert.strictEqual(followingRes.status, 200);
-		assert.strictEqual(Array.isArray(followingRes.body), true);
-		assert.strictEqual(followersRes.status, 200);
-		assert.strictEqual(Array.isArray(followersRes.body), true);
-	}));
+		it('shows followers and following to self', visible(alice));
+		it('shows followers and following to a follower', visible(follower));
+		it('shows followers and following to a non-follower', visible(bob));
+		it('shows followers and following when unauthenticated', visible(null));
 
-	it('ffVisibility が followers なユーザーのフォロー/フォロワーを非フォロワーが見れない', async(async () => {
-		await request('/i/update', {
-			ffVisibility: 'followers',
-		}, alice);
+		it('provides followers in ActivityPub representation', async () => {
+			const followingRes = await simpleGet(`/users/${alice.id}/following`, 'application/activity+json');
+			const followersRes = await simpleGet(`/users/${alice.id}/followers`, 'application/activity+json');
+			assert.strictEqual(followingRes.status, 200);
+			assert.strictEqual(followersRes.status, 200);
+		});
+	});
 
-		const followingRes = await request('/users/following', {
-			userId: alice.id,
-		}, bob);
-		const followersRes = await request('/users/followers', {
-			userId: alice.id,
-		}, bob);
+	describe('followers visibility', () => {
+		before(async () => {
+			await request('/i/update', {
+				ffVisibility: 'followers',
+			}, alice);
+		});
 
-		assert.strictEqual(followingRes.status, 400);
-		assert.strictEqual(followersRes.status, 400);
-	}));
+		it('shows followers and following to self', visible(alice));
+		it('shows followers and following to a follower', visible(follower));
+		it('hides followers and following from a non-follower', hidden(bob));
+		it('hides followers and following when unauthenticated', hidden(null));
 
-	it('ffVisibility が followers なユーザーのフォロー/フォロワーをフォロワーが見れる', async(async () => {
-		await request('/i/update', {
-			ffVisibility: 'followers',
-		}, alice);
+		it('hides followers from ActivityPub representation', async () => {
+			const followingRes = await simpleGet(`/users/${alice.id}/following`, 'application/activity+json').catch(res => ({ status: res.statusCode }));
+			const followersRes = await simpleGet(`/users/${alice.id}/followers`, 'application/activity+json').catch(res => ({ status: res.statusCode }));
+			assert.strictEqual(followingRes.status, 403);
+			assert.strictEqual(followersRes.status, 403);
+		});
+	});
 
-		await request('/following/create', {
-			userId: alice.id,
-		}, bob);
+	describe('private visibility', () => {
+		before(async () => {
+			await request('/i/update', {
+				ffVisibility: 'private',
+			}, alice);
+		});
 
-		const followingRes = await request('/users/following', {
-			userId: alice.id,
-		}, bob);
-		const followersRes = await request('/users/followers', {
-			userId: alice.id,
-		}, bob);
+		it('shows followers and following to self', visible(alice));
+		it('hides followers and following from a follower', hidden(follower));
+		it('hides followers and following from a non-follower', hidden(bob));
+		it('hides followers and following when unauthenticated', hidden(null));
 
-		assert.strictEqual(followingRes.status, 200);
-		assert.strictEqual(Array.isArray(followingRes.body), true);
-		assert.strictEqual(followersRes.status, 200);
-		assert.strictEqual(Array.isArray(followersRes.body), true);
-	}));
-
-	it('ffVisibility が private なユーザーのフォロー/フォロワーを自分で見れる', async(async () => {
-		await request('/i/update', {
-			ffVisibility: 'private',
-		}, alice);
-
-		const followingRes = await request('/users/following', {
-			userId: alice.id,
-		}, alice);
-		const followersRes = await request('/users/followers', {
-			userId: alice.id,
-		}, alice);
-
-		assert.strictEqual(followingRes.status, 200);
-		assert.strictEqual(Array.isArray(followingRes.body), true);
-		assert.strictEqual(followersRes.status, 200);
-		assert.strictEqual(Array.isArray(followersRes.body), true);
-	}));
-
-	it('ffVisibility が private なユーザーのフォロー/フォロワーを他人が見れない', async(async () => {
-		await request('/i/update', {
-			ffVisibility: 'private',
-		}, alice);
-
-		const followingRes = await request('/users/following', {
-			userId: alice.id,
-		}, bob);
-		const followersRes = await request('/users/followers', {
-			userId: alice.id,
-		}, bob);
-
-		assert.strictEqual(followingRes.status, 400);
-		assert.strictEqual(followersRes.status, 400);
-	}));
-
-	describe('AP', () => {
-		it('ffVisibility が public 以外ならばAPからは取得できない', async(async () => {
-			{
-				await request('/i/update', {
-					ffVisibility: 'public',
-				}, alice);
-
-				const followingRes = await simpleGet(`/users/${alice.id}/following`, 'application/activity+json');
-				const followersRes = await simpleGet(`/users/${alice.id}/followers`, 'application/activity+json');
-				assert.strictEqual(followingRes.status, 200);
-				assert.strictEqual(followersRes.status, 200);
-			}
-			{
-				await request('/i/update', {
-					ffVisibility: 'followers',
-				}, alice);
-
-				const followingRes = await simpleGet(`/users/${alice.id}/following`, 'application/activity+json').catch(res => ({ status: res.statusCode }));
-				const followersRes = await simpleGet(`/users/${alice.id}/followers`, 'application/activity+json').catch(res => ({ status: res.statusCode }));
-				assert.strictEqual(followingRes.status, 403);
-				assert.strictEqual(followersRes.status, 403);
-			}
-			{
-				await request('/i/update', {
-					ffVisibility: 'private',
-				}, alice);
-
-				const followingRes = await simpleGet(`/users/${alice.id}/following`, 'application/activity+json').catch(res => ({ status: res.statusCode }));
-				const followersRes = await simpleGet(`/users/${alice.id}/followers`, 'application/activity+json').catch(res => ({ status: res.statusCode }));
-				assert.strictEqual(followingRes.status, 403);
-				assert.strictEqual(followersRes.status, 403);
-			}
-		}));
+		it('hides followers from ActivityPub representation', async () => {
+			const followingRes = await simpleGet(`/users/${alice.id}/following`, 'application/activity+json').catch(res => ({ status: res.statusCode }));
+			const followersRes = await simpleGet(`/users/${alice.id}/followers`, 'application/activity+json').catch(res => ({ status: res.statusCode }));
+			assert.strictEqual(followingRes.status, 403);
+			assert.strictEqual(followersRes.status, 403);
+		});
 	});
 });
